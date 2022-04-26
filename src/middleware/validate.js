7bc5d51fc626ce9baf32e126cd60a918c9d401f9
devriwirlandika <wirlandikadevri@gmail.com>
2022-04-26 20:02:06 +0700

    middleware validate

---
+++ src/middleware/validate.js
@@ -0,0 +1,21 @@
+const respone = require('../helpers/response')
+const jwt = require('jsonwebtoken')
+
+const validateToken = (req, res, next) => {
+    const { auth_token } = req.headers
+
+    if (!auth_token) {
+        return respone(res, 401, { msg: 'silahkan login dulu' })
+    }
+
+    jwt.verify(auth_token, process.env.JWT_KEYS, (err, decode) => {
+        if (err) {
+            return respone(res, 401, err)
+        }
+
+        req.users = decode
+        next()
+    })
+}
+
+module.exports = validateToken
--- src/routers/movies.js
+++ src/routers/movies.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const routers = express.Router()
 const controller = require('../controllers/movie')
+const validate = require('../middleware/validate')
 
-routers.get('/', controller.getData)
+routers.get('/', validate, controller.getData)
 routers.get('/all', controller.getAll)
 routers.get('/search', controller.getTitle)
 routers.get('/:id', controller.getMovie)
-routers.post('/', controller.createData)
-routers.put('/:id', controller.updateData)
-routers.delete('/:id', controller.deleteMovie)
+routers.post('/', validate, controller.createData)
+routers.put('/:id', validate, controller.updateData)
+routers.delete('/:id', validate, controller.deleteMovie)
 
 module.exports = routers
