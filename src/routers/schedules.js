29ab5c16cbfe6b248b2fe4f9fa8a11a710cb8776
devriwirlandika <wirlandikadevri@gmail.com>
2022-05-10 15:14:05 +0700

    added validate and upload image in route schedule

--- src/routers/schedules.js
+++ src/routers/schedules.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const routers = express.Router()
 const controller = require('../controllers/schedule')
+const validate = require('../middleware/validate')
 
 routers.get('/', controller.getAll)
 routers.get('/:id', controller.getData)
-routers.post('/', controller.createData)
-routers.put('/:id', controller.updateData)
-routers.delete('/:id', controller.deleteData)
+routers.post('/', validate(['admin']), controller.createData)
+routers.put('/:id', validate(['admin']), controller.updateData)
+routers.delete('/:id', validate(['admin']), controller.deleteData)
 
 module.exports = routers
