c6393f1f29b62ae9ab2a7527f86644ce24a9a507
devriwirlandika <wirlandikadevri@gmail.com>
2022-05-10 15:15:22 +0700

    added validate and upload images in route booking

--- src/routers/bookings.js
+++ src/routers/bookings.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const routers = express.Router()
 const controller = require('../controllers/booking')
+const validate = require('../middleware/validate')
 
-routers.get('/', controller.getAll)
-routers.post('/', controller.createData)
-routers.get('/:id', controller.getData)
-routers.put('/:id', controller.updateData)
-routers.delete('/:id', controller.deleteData)
+routers.get('/', validate(['admin']), controller.getAll)
+routers.post('/', validate(['admin', 'user']), controller.createData)
+routers.get('/:id', validate(['admin', 'user']), controller.getData)
+routers.put('/:id', validate(['admin', 'user']), controller.updateData)
+routers.delete('/:id', validate(['admin', 'user']), controller.deleteData)
 
 module.exports = routers
